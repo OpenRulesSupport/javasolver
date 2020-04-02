6b419c41dc226ad01e63f4af8bb338246f827117
Jacob Feldman <jacobfeldman@openrules.com>
2020-04-02 10:55:16 -0400

    Integrated with JSR331 2.0.1 solvers available from the MVN repository

--- com.javasolver.samples/run.bat
+++ com.javasolver.samples/run.bat
@@ -1,4 +1,3 @@
 if "%CLASS_NAME%" == "" set CLASS_NAME=com.javasolver.samples.ProblemZoo
 if "%SOLVER%" == "" set SOLVER=Constrainer
-rem call mvn -f pom%SOLVER%.xml compile exec:java -Dexec.mainClass=%CLASS_NAME% -q
 call mvn -P %SOLVER% compile exec:java -Dexec.mainClass=%CLASS_NAME% -q
