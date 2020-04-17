e20328d3b9ccbf3505c27f82614103a9abf9a46b
Jacob Feldman <jacobfeldman@openrules.com>
2020-04-17 11:12:03 -0400

    added more solver choices

--- com.javasolver.samples/runZoo.bat
+++ com.javasolver.samples/runZoo.bat
@@ -1,8 +1,14 @@
 set CLASS_NAME=com.javasolver.samples.ProblemZoo
+rem ====== CONSTRAINT SOLVERS ======
+set SOLVER=Sugar
+rem set SOLVER=Constrainer
+rem set SOLVER=Choco
+rem set SOLVER=JSetL
+rem ====== LINEAR SOLVERS ======
 rem set SOLVER=Scip
 rem set SOLVER=GLPK
-rem set SOLVER=Constrainer
-set SOLVER=JSetL
+rem set SOLVER=Coin
+
 @echo off
 cd %~dp0
 call .\run
