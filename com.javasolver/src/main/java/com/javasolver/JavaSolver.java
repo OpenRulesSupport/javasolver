b92c30e6fc0e3156e5b16d2831f4abadac83c4d7
Jacob Feldman <jacobfeldman@openrules.com>
2024-04-17 15:55:39 -0400

    2.2.0 => 2.3.0 to sync with JSR331 2.3.0

--- com.javasolver/src/main/java/com/javasolver/JavaSolver.java
+++ com.javasolver/src/main/java/com/javasolver/JavaSolver.java
@@ -13,7 +13,7 @@ import javax.constraints.VarReal;
 
 public class JavaSolver {
 	
-	static final String RELEASE = "Java Solver Release 2.2.0 (build of Dec 19, 2021)";
+	static final String RELEASE = "Java Solver Release 2.3.0 (build of April 12, 2024)";
 
 	protected Problem csp; // used by all subclasses
 	protected Var objectiveVar;
